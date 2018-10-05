27c3aa5c351ad8c6dd212ec324b3167466a655a5
anelly1201 <anellys0303@gmail.com>
2018-10-05 14:06:20 -0500

    Primer repositorio y primer commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+	$method = $_SERVER['REQUEST_METHOD'];
+	//POST METHOD
+	if ($method == 'POST') {
+		$requestBody = file_get_contents('php://input');
+		$json = json_decode($requestBody);
+
+		$text = $json->queryResult->parameters->text;
+
+		switch ($text) {
+			case 'hi':
+				$speech = "Hi, nice to meet u";
+				break;
+			case 'bye':
+				$speech = "¡Bye!";
+				break;
+			default:
+				$speech = "Sorry, i did not understand u ";
+				break;
+		}
+
+		$response = new \stdClass();
+		#$response->speech = $speech;
+		$response->fulfillmentText = $speech;
+		$response->source = "webhook";
+		echo json_encode($response);
+
+	}
+	else{
+		echo "Method not allowed";
+	}
+?>
